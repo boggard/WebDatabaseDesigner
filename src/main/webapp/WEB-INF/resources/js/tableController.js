372e2e38ae9fa21fd94f3faea10ddac7040dc62d
boggard <varyanik2@gmail.com>
2017-04-27 07:21:18 +0300

    Валидация имени и типа полей

--- src/main/webapp/WEB-INF/resources/js/tableController.js
+++ src/main/webapp/WEB-INF/resources/js/tableController.js
@@ -5,17 +5,17 @@ app.controller('tableController', ['$uibModalInstance', '$scope', '$sce', 'table
 
         self.tables = tables;
 
-        self.table  = {
+        self.table = {
             name: 'Table',
             fields: [],
             foreignKeys: []
         };
 
-        self.addField  = function () {
+        self.addField = function () {
             self.table.fields.push(
                 {
-                    name: 'field',
-                    type: 'BIGINT',
+                    name: '',
+                    type: '',
                     primaryKey: false,
                     foreignKey: false
                 }
@@ -34,26 +34,52 @@ app.controller('tableController', ['$uibModalInstance', '$scope', '$sce', 'table
         };
 
         self.addTable = function () {
+            var valid = true;
+            self.table.fields.forEach(function (item, i, arr) {
+                if (!self.checkFieldValid(item)) {
+                    valid = false;
+                }
+            });
+            if (!valid) {
+                return;
+            }
             modalInstance.close(self.table);
         };
 
-    self.actionForeignKey = function (field) {
-        if (field.foreignKey) {
-            self.table.foreignKeys.push(
-                {
-                    fieldName: field.name
+        self.actionForeignKey = function (field) {
+            if (field.foreignKey) {
+                if (!self.checkFieldValid(field)) {
+                    field.foreignKey = false;
+                    return;
                 }
-            )
-        } else {
-            var elem = self.table.foreignKeys.find(function (element, index, array) {
-                return element.fieldName === field.name;
-            });
-            var index = self.table.foreignKeys.indexOf(elem);
-            if (index !== -1) {
-                self.table.foreignKeys.splice(index, 1);
+                self.table.foreignKeys.push(
+                    {
+                        fieldName: field.name
+                    }
+                )
+            } else {
+                var elem = self.table.foreignKeys.find(function (element, index, array) {
+                    return element.fieldName === field.name;
+                });
+                var index = self.table.foreignKeys.indexOf(elem);
+                if (index !== -1) {
+                    self.table.foreignKeys.splice(index, 1);
+                }
             }
-        }
-    };
+        };
+
+        self.checkFieldValid = function (field) {
+            if (!field.name.trim()) {
+                self.errorMessage = "Имя поля таблицы не должно быть пустым";
+                return false;
+            }
+            if (!field.type.trim()) {
+                self.errorMessage = "Тип поля таблицы не должен быть пустым";
+                return false;
+            }
+            self.errorMessage = null;
+            return true;
+        };
 
         return self;
     }]);
